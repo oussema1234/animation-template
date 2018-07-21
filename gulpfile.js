090ee5a0234b65a0eee98dfaae864bce65732277
oussema <oussemabarka9@gmail.com>
2018-07-21 15:18:35 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+  
+  var gulp = require('gulp'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename');
+      
+  gulp.task('build:full', function() {
+    return gulp.src('./src/*')
+      .pipe(rename('jquery.particles.js'))
+      .pipe(gulp.dest('./dist/'));
+  });
+  
+  gulp.task('build:compressed', function() {
+    return gulp.src('./src/*')
+      .pipe(uglify())
+      .pipe(rename('jquery.particles.min.js'))
+      .pipe(gulp.dest('./dist/'));
+  });
+  
+  gulp.task('build', ['build:full', 'build:compressed']);
+}());
+
